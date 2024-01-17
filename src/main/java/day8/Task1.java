1a7cbe919902b1fc44ac326c8c485f2f001e3b29
ArtyomVlasov <www.tema.ru.a@mail.ru>
2024-01-17 20:18:03 +0300

    change day 8

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,22 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        StringBuilder string = new StringBuilder();
+        for(int i = 0; i <20001; i++) {
+            string.append(i + " ");
+        }
+        System.out.println(string.toString());
+        long stopTime = System.currentTimeMillis();
+        System.out.println("Длитеьность работы, в мс.: " + (stopTime - startTime));
 
+        long start = System.currentTimeMillis();
+        String str = "";
+        for(int i = 0; i < 20001; i++) {
+            str += i + " ";
+        }
+        System.out.println(string);
+        long stop = System.currentTimeMillis();
+        System.out.println("Длитеьность работы, в мс.: " + (stop - start));
     }
 }
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -2,6 +2,7 @@ package day8;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        Airplane plane = new Airplane("Боинг", 1991, 53, 150000);
+        System.out.println(plane);
     }
 }
