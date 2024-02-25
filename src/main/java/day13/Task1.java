4086f97e493aa2aff51b61a6ebba60e7f71a5a8f
ArtyomVlasov <www.tema.ru.a@mail.ru>
2024-02-25 15:16:20 +0300

    Add day 13

--- src/main/java/day13/Task1.java
+++ src/main/java/day13/Task1.java
@@ -2,6 +2,29 @@ package day13;
 
 public class Task1 {
     public static void main(String[] args) {
-
+User user1 = new User("Bob");
+User user2 = new User("John");
+User user3 = new User("Logan");
+user1.sendMessage(user2,"Привет, John!");
+        user1.sendMessage(user2,"Как дела?");
+        user2.sendMessage(user1,"Привет!");
+        user2.sendMessage(user1,"Хорошо!");
+        user2.sendMessage(user1,"У тебя как?");
+        user3.sendMessage(user1,"Привет, Bob!");
+        user3.sendMessage(user1,"Как оно?");
+        user3.sendMessage(user1,"Все хорошо?");
+        user1.sendMessage(user3,"Привет, Logan!");
+        user1.sendMessage(user3,"Все отлично");
+        user1.sendMessage(user3,"У тебя как?");
+        user3.sendMessage(user1,"Отлично");
+        MessageDatabase.showDialog(user1,user3);
+        user1.subscribe(user3);
+            System.out.println(user1.getSubscriptions());
+            System.out.println(user1.isSubscribed(user3));
+            System.out.println(user1.isFriend(user3));
+            user3.subscribe(user1);
+            System.out.println(user3.getSubscriptions());
+            System.out.println(user3.isSubscribed(user1));
+            System.out.println(user1.isFriend(user3));
     }
 }
---
+++ src/main/java/day13/User.java
@@ -0,0 +1,44 @@
+package day13;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private String username;
+    private List<User> subscriptions;
+    public User(String username){
+        this.username=username;
+        this.subscriptions = new ArrayList<>();
+    }
+    public void subscribe(User user){
+        subscriptions.add(user);
+    }
+    public boolean isSubscribed(User user){
+        if(getSubscriptions().contains(user)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean isFriend(User user){
+        if(this.getSubscriptions().contains(user) && user.getSubscriptions().contains(this)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public void sendMessage(User user, String text){
+MessageDatabase.addNewMessage(this,user,text);
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    public List<User> getSubscriptions() {
+        return subscriptions;
+    }
+    @Override
+    public String toString() {
+        return username;
+    }
+}
